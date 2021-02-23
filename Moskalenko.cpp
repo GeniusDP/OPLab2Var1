68437dc943d97d1f56bb12cb2f6d93b46c0a28a5
Vladyslav2020 <vmskl2020@gmail.com>
2021-02-23 15:43:23 +0200

    Добавление parsingFiles() и splitStringByComma()

--- Moskalenko.cpp
+++ Moskalenko.cpp
@@ -1,1 +1,51 @@
 #include <bits/stdc++.h>
+
+using namespace std;
+
+struct Country{
+    char name[100]={};
+    int points[100]={};
+    int totalScore=0;
+};
+
+vector< string > splitStringByComma(string str)
+{
+    vector< string > answer;
+    string current;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] != ',')
+            current += str[i];
+        else
+        {
+            answer.push_back(current);
+            current = "";
+        }
+    }
+    if (current != "")
+        answer.push_back(current);
+    return answer;
+}
+
+void parsingFiles(Country* contr, int* numberOfCountries, char (*namesOfFiles)[100], int numberOfFiles)
+{
+    numberOfCountries = 0;
+    for (int i = 0; i < numberOfFiles; i++)
+    {
+        string nameFile = string(namesOfFiles[i]);
+        ifstream input(namesOfFiles[i]);
+        int N;
+        input >> N;
+        for (int j = 0; j < N; j++)
+        {
+            string data;
+            getline(input, data);
+            vector< string > items = splitStringByComma(data);
+            for (int k = 0; k < items[0].size() && k < 100; k++)
+                contr[*numberOfCountries].name[k] = items[0][k];
+            for (int k = 1; k < items.size(); k++)
+                contr[*numberOfCountries].points[k - 1] = stoi(items[k]);
+            (*numberOfCountries)++;
+        }
+    }
+}
